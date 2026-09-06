f0daa5abfa1caba5a5b9f53c07b062a00743e6e3
jLemmings <jsohua@hemmings.ch>
2026-09-06 17:42:55 +0200

    fix windows build

--- scripts/build_libdivecomputer.sh
+++ scripts/build_libdivecomputer.sh
@@ -6,7 +6,11 @@ if [ -f Makefile ]; then make clean; fi
 # Uses installed libusb/hidapi when available. All device protocols stay upstream.
 autoreconf --install --force
 configure_args=(--prefix="$ROOT/vendor/native" --disable-static)
-case "$(uname -s)" in MINGW*|MSYS*) configure_args+=(--host="$(gcc -dumpmachine)");; esac
+case "$(uname -s)" in
+	MINGW*|MSYS*)
+		configure_args+=(--host="$(gcc -dumpmachine)" --without-libusb --without-hidapi)
+		;;
+esac
 ./configure "${configure_args[@]}"
 make -j"${JOBS:-4}"
 make install
